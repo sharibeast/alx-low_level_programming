2cec63839b10565446a3e490e8ea1c3c62443313
Sharif <sharibeast@gmail.com>
2023-06-15 15:05:52 -0700

    adding changes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'program that prints exactly giveen sentence'
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	const char message *  = "and that piece of art is 
+		useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, strlen(message));
+	return (1);
+}

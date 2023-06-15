be5f98e042f2ee819e4372c9ab4d27ab9ac36ee3
Sharif <sharibeast@gmail.com>
2023-06-15 14:09:20 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'this is entry point of our program whre th program starts'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

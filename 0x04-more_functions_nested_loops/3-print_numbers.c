1372a504eb778370e4d69f06f83b1b7384be7dc8
Sharif <sharibeast@gmail.com>
2023-06-23 18:42:47 -0700

    fix: 3 error

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,8 @@
 
 /**
  *print_numbers - Entry point
- *Description: 'function that prints the numbers, from 0 to 9'
+ *Description: 'function that prints the numbers
+ *from 0 to 9, followed by a new line.'
  *Return: Integer Value 0
  */
 

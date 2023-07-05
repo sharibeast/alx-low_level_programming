5a6bf9ce32da27c02468c4f82f163f97d195bf1d
Sharif <georginasharif@gmail.com>
2023-07-06 00:40:13 +0300

    adding stlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - function that returns the length of a string.
+ * @s: pointer
+ * Return: int length of string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}

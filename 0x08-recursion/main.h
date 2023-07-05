204a3c3bb2d5a3689fb44f32c06d7f34fe6bdcd9
Sharif <georginasharif@gmail.com>
2023-07-06 00:39:16 +0300

    adding headers

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,4 +4,9 @@
 int is_prime_number(int n);
 int _putchar(char c);
 int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
+
 #endif

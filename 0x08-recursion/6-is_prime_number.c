7c503cc276ba074199b997cdca003f753982a5b6
Sharif <georginasharif@gmail.com>
2023-07-06 00:01:39 +0300

    fix: betty style codee

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+
+
+/**
+ * prime_helper - function that helps to calculate prime numbers
+ * @n: number
+ * @i: interger
+ *
+ * Return: integer.
+ */
+
 int prime_helper(int n, int i)
 {
 	if (i == 1)
@@ -8,12 +18,19 @@ int prime_helper(int n, int i)
 	}
 	else if (i > 0 && n % i == 0)
 	{
-		return 0;
+		return (0);
 	}
 	else
 		return (prime_helper(n, i - 1));
 }
 
+
+/**
+ * is_prime_number - function that returns 1 if the input integer
+ *  is a prime number, otherwise return 0
+ * @n: number
+ * Return: integer.
+ */
 int is_prime_number(int n)
 {
 	if (n <= 1)

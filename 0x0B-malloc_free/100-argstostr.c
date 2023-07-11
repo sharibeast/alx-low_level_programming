d38ccf534c0f4cce28f3534e57b8a5d6495471db
Sharif <georginasharif@gmail.com>
2023-07-12 02:18:19 +0300

    fix 101

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -6,12 +6,11 @@
  * @ac: string to be converted
  * @av: string to be converted
  *
- * Return: the int converted from the string
+ * Return: a pointer to a new string, or NULL if it fails
  */
-
 char *argstostr(int ac, char **av)
 {
-	size_t total_len, i, length;
+	size_t total_length, index, length;
 	char *result;
 	int i;
 
@@ -20,28 +19,28 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	}
 
-	total_len = 0;
+	total_length = 0;
 	for (i = 0; i < ac; i++)
 	{
-		total_len += strlen(av[i]) + 1;
+		total_length += strlen(av[i]) + 1;
 	}
 
-	result = (char *)malloc((total_len + 1) * sizeof(char));
+	result = (char *)malloc((total_length + 1) * sizeof(char));
 	if (result == NULL)
 	{
 		return (NULL);
 	}
 
-	i = 0;
+	index = 0;
 	for (i = 0; i < ac; i++)
 	{
 		length = strlen(av[i]);
-		strcpy(result + i, av[i]);
-		i += length;
-		result[i++] = '\n';
+		strcpy(result + index, av[i]);
+		index += length;
+		result[index++] = '\n';
 	}
 
-	result[total_len] = '\0';
+	result[total_length] = '\0';
 
 	return (result);
 }

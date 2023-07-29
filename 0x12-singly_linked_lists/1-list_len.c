3c940410c892484654dcee7f5b3b3ea9b06567fd
Sharif <georginasharif@gmail.com>
2023-07-29 03:59:04 +0300

    fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -12,13 +12,13 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t node_count = 0;
+	size_t node = 0;
 
 	while (h != NULL)
 	{
-		node_count++;
+		node++;
 		h = h->next;
 	}
 
-	return (node_count);
+	return (node);
 }

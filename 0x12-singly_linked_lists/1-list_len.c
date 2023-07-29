b5f0639fc7dbcdad1702bcd9ce030ed92b46f6ba
Sharif <georginasharif@gmail.com>
2023-07-29 03:58:27 +0300

    feat: changes

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
  * list_len - function that returns the number of elements
@@ -10,12 +12,13 @@
 
 size_t list_len(const list_t *h)
 {
-	unsigned int i = 0;
+	size_t node_count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
+		node_count++;
 		h = h->next;
-		i++;
 	}
-	return (i);
+
+	return (node_count);
 }

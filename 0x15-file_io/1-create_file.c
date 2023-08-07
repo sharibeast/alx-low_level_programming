9b5f4f6e847c918f0d9be8c3e63eacc95706bc16
Sharif <georginasharif@gmail.com>
2023-08-08 02:18:58 +0300

    changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,22 +1,24 @@
+
 #include "main.h"
 
 /**
  * create_file - function that creates a file.
  * @filename:  name of the file to create and text_content
  * @text_content: content of file
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
 	int file;
-            int file_write;
-            int file_length;
+	int file_write;
+	int file_length;
 
 	file_length = 0;
+
 	if (!filename)
-            {
+	{
 		return (-1);
-            }
+	}
 	if (text_content)
 	{
 		for (; text_content[file_length];)
@@ -25,9 +27,9 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	file_write = write(file, text_content, file_length);
 	if (file == -1 || file_write == -1)
-            {
+	{
 		return (-1);
-            }
+	}
 	close(file);
 	return (1);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,82 +1,114 @@
 #include "main.h"
-#define MAX_SIZE 1024
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * createBuffer - create a 1024 bytes buffer
- * @file_buff: buffer file
- * Return: created buffer
- */
-char *createBuffer(char *file_buff)
-{
-	char *buffer;
-
-	buffer = (char *)malloc(sizeof(char) * MAX_SIZE);
-	if (!buffer)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_buff);
-		exit(99);
-	}
-	return (buffer);
-}
-
-
-/**
- * f_close - function to close a file when file descriptor cant closed
- * @file_d: file descriptor to be closed
- */
-void f_close(int file_d)
-{
-	int d;
-
-	d = close(file_d);
-	if (d == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
-		exit(100);
-	}
-}
-
-
-/**
- * main - main function
- * @argc: number of arguments
- * @argv: argument lists
+ * checkArgumentsNumber - checks for the correct number of arguments
+ * @args: number of arguments
  *
- * Return: Always 0
+ * Return: void
  */
-int main(int argc, char *argv[])
+void checkArgumentsNumber(int args)
 {
-	char *bufferCreated;
-	int from, to, fileRead, file_write;
-
-	if (argc != 3)
+	if (args != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+}
 
-	fileRead = read(from, bufferCreated, MAX_SIZE);
-	bufferCreated = createBuffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while (fileRead > 0)
+/**
+ * fileFromDoesntExist - checks that file_from exists and can be read
+ * @check: checks if true of false
+ * @source: file_from name
+ * @from: file descriptor of file_from, or -1
+ * @to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+void fileFromDoesntExist(ssize_t check, char *source, int from, int to)
+{
+	if (check == -1)
 	{
-		if (fileRead == -1 || from == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		file_write = write(to, bufferCreated, fileRead);
-		if (to == -1 || file_write == 1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-		fileRead = read(from, bufferCreated, MAX_SIZE);
-		to = open(argv[2], O_WRONLY | O_APPEND);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", source);
+		if (from != -1)
+			close(from);
+		if (to != -1)
+			close(to);
+		exit(98);
 	}
-	free(bufferCreated);
-	close(from);
-	close(to);
+}
+
+/**
+ * createFail - checks that file_to was created and/or can be written to
+ * @check: checks if true of false
+ * @destination: file_to name
+ * @d_from: file descriptor of file_from, or -1
+ * @d_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
+void createFail(ssize_t check, char *destination, int d_from, int d_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", destination);
+		if (d_from != -1)
+			close(d_from);
+		if (d_to != -1)
+			close(d_to);
+		exit(99);
+	}
+}
+
+/**
+ * check100 - checks that file descriptors were closed properly
+ * @check: checks if true or false
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void check100(int check, int fd)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
+ * main - opies the content of a file to another file.
+ * @args: number of arguments passed
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 on success
+ */
+int main(int args, char *argv[])
+{
+	int fd_from, fd_to, close_to, close_from;
+	ssize_t lenr, lenw;
+	char buffer[1024];
+	mode_t file_perm;
+
+	checkArgumentsNumber(args);
+	fd_from = open(argv[1], O_RDONLY);
+	fileFromDoesntExist((ssize_t)fd_from, argv[1], -1, -1);
+	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	createFail((ssize_t)fd_to, argv[2], fd_from, -1);
+	lenr = 1024;
+	while (lenr == 1024)
+	{
+		lenr = read(fd_from, buffer, 1024);
+		fileFromDoesntExist(lenr, argv[1], fd_from, fd_to);
+		lenw = write(fd_to, buffer, lenr);
+		if (lenw != lenr)
+			lenw = -1;
+		createFail(lenw, argv[2], fd_from, fd_to);
+	}
+	close_to = close(fd_to);
+	close_from = close(fd_from);
+	check100(close_to, fd_to);
+	check100(close_from, fd_from);
 	return (0);
 }

a8b3a887b1d97dc9c3cedd6dfc2b05514221f463
Sharif <georginasharif@gmail.com>
2023-08-08 02:21:32 +0300

    changes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,11 +8,9 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fileOpen;
-	int fileWrite;
-	int len;
+	int a, b, length;
 
-	len = 0;
+	length = 0;
 
 	if (filename == NULL)
 	{
@@ -21,19 +19,19 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (length = 0; text_content[length];)
+			length++;
 	}
 
-	fileWrite = write(fileOpen, text_content, len);
-	fileOpen = open(filename, O_WRONLY | O_APPEND);
+	a = open(filename, O_WRONLY | O_APPEND);
+	b = write(a, text_content, length);
 
-	if (fileOpen == -1 || fileWrite == -1)
+	if (a == -1 || b == -1)
 	{
 		return (-1);
 	}
 
-	close(fileOpen);
+	close(a);
 
 	return (1);
 }

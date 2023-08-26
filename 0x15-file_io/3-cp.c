604f274def3cf1c61a7855d053f20e691eae8122
sharibeast <georginasharif@gmail.com>
2023-08-26 11:23:34 +0300

    cp file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,21 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 
+
+
 /**
  * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
+ * @dest_from: copy from.
+ * @dest_to: copy file to.
  * @argv: arguments vector.
- * Return: no return.
+ * Return: void.
  */
-void error_file(int file_from, int file_to, char *argv[])
+void error_file(int dest_from, int dest_to, char *argv[])
 {
-	if (file_from == -1)
+	if (dest_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (file_to == -1)
+	if (dest_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
@@ -23,14 +25,16 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - check the code for Holberton School students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
+ * main - fuction that copy file to another dest
+ * @argc: arguments.
+ * @argv: arguments ptr.
+ * Return: Always success.
  */
+
+
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, err_close;
+	int dest_from, dest_to, err_close;
 	ssize_t nchars, nwr;
 	char buf[1024];
 
@@ -40,32 +44,32 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
+	dest_from = open(argv[1], O_RDONLY);
+	dest_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(dest_from, dest_to, argv);
 
 	nchars = 1024;
 	while (nchars == 1024)
 	{
-		nchars = read(file_from, buf, 1024);
+		nchars = read(dest_from, buf, 1024);
 		if (nchars == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
+		nwr = write(dest_to, buf, nchars);
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
 
-	err_close = close(file_from);
+	err_close = close(dest_from);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_from);
 		exit(100);
 	}
 
-	err_close = close(file_to);
+	err_close = close(dest_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_from);
 		exit(100);
 	}
 	return (0);

ee3993e8ffa9be3985afe876f981a90889c7a5a5
sharibeast <georginasharif@gmail.com>
2023-09-15 05:22:35 +0300

    fx

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,13 +9,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t m = 0;
+	size_t x = 0;
 	const dlistint_t *tmp = h;
 
 	while (tmp != NULL)
 	{
 		printf("%d\n", tmp->n);
 		tmp = tmp->next;
-		m++;
+		x++;
 	}
+	return (x);
 }
